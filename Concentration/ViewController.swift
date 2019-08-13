a83fb122927de607a4b54f12eddd1816cb813b0d
Manh Hoang Le <hoanglemanh1993@gmail.com>
2019-08-13 15:31:11 +0200

    Storyboard angepasst

--- Concentration/ViewController.swift
+++ Concentration/ViewController.swift
@@ -109,20 +109,6 @@ class ViewController: UIViewController {
             return "?"
         }*/
     }
-    
-    
-    
-
-    /**
-    func flipCard(withEmoji emoji: String, on button: UIButton){
-        if(button.currentTitle == emoji){
-            button.setTitle("", for: UIControl.State.normal)
-            button.backgroundColor = #colorLiteral(red: 1, green: 0.5763723254, blue: 0, alpha: 1)
-        } else {
-            button.setTitle(emoji, for: UIControl.State.normal)
-            button.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
-        }
-    }*/
 }
 
 
